e357aba39f3fa28d0c04c5609196bca77704afaa
Hugo Petament <h.petament@icloud.com>
2023-09-17 01:22:39 +0200

    implementation of is writing function

--- app/javascript/channels/channel_test_channel.js
+++ app/javascript/channels/channel_test_channel.js
@@ -1,6 +1,9 @@
 import consumer from "channels/consumer"
 
 let clientId = Math.random().toString(36).substring(2, 15);
+const input = document.getElementById('input');
+const writingStatus = document.getElementById('writing-status');
+let writingTimeout;
 
 consumer.subscriptions.create("ChannelTestChannel", {
   connected() {
@@ -17,8 +20,17 @@ consumer.subscriptions.create("ChannelTestChannel", {
     if (data.client_id !== clientId) {
       const audio = new Audio('messages_alert.mp3');
       audio.play();
+
+      if (data.status === 'writing') {
+        writingStatus.innerHTML = `<p id="writing-status"><strong>${data.ip_address} is writing...</p>`;
+      } else if (data.status === 'stopped_writing') {
+        writingStatus.innerHTML = ``;
+      }
     }
-    document.getElementById('messages').insertAdjacentHTML('afterbegin', `<p><strong>${data.ip_address} :</strong> ${data.message}</p>`)
+    if (data.message) {
+      document.getElementById('messages').insertAdjacentHTML('afterbegin', `<p><strong>${data.ip_address} :</strong> ${data.message}</p>`)
+      writingStatus.innerHTML = ``;
+    }
   }
 });
 
@@ -26,7 +38,19 @@ function sendMessage(message) {
   consumer.subscriptions.subscriptions[0].send({ message: message, client_id: clientId });
 }
 
+function sendWriting() {
+  consumer.subscriptions.subscriptions[0].send({ status: 'writing', client_id: clientId });
+}
+
 document.getElementById('my-button').addEventListener('click', () => {
-  const input = document.getElementById('input');
   sendMessage(input.value);
+  input.value = '';
+});
+
+input.addEventListener('input', () => {
+  clearTimeout(writingTimeout);  // annuler le précédent timeout
+  sendWriting();
+  writingTimeout = setTimeout(() => {
+    consumer.subscriptions.subscriptions[0].send({ status: 'stopped_writing', client_id: clientId });
+  }, 1500);
 });
