b5bb69db005725ffaea3ae2a31cf895c9214127e
Hugo Petament <h.petament@icloud.com>
2023-09-17 00:17:45 +0200

    disabled sound if message is the client message

--- app/javascript/channels/channel_test_channel.js
+++ app/javascript/channels/channel_test_channel.js
@@ -1,9 +1,11 @@
 import consumer from "channels/consumer"
 
+let clientId = Math.random().toString(36).substring(2, 15);
+
 consumer.subscriptions.create("ChannelTestChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
-    console.log("Connected to MyChannel");
+    console.log(`Connected to MyChannel as : ${clientId}`);
   },
 
   disconnected() {
@@ -12,14 +14,16 @@ consumer.subscriptions.create("ChannelTestChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    const audio = new Audio('messages_alert.mp3');
-    audio.play();
+    if (data.client_id !== clientId) {
+      const audio = new Audio('messages_alert.mp3');
+      audio.play();
+    }
     document.getElementById('messages').insertAdjacentHTML('afterbegin', `<p><strong>${data.ip_address} :</strong> ${data.message}</p>`)
   }
 });
 
 function sendMessage(message) {
-  consumer.subscriptions.subscriptions[0].send({ message: message });
+  consumer.subscriptions.subscriptions[0].send({ message: message, client_id: clientId });
 }
 
 document.getElementById('my-button').addEventListener('click', () => {
